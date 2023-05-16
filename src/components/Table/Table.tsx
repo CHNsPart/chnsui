7abda8266ebfe04ae9f4e99db231dfaa89cf9d76
chnspart <imchn24@gmail.com>
2023-05-17 02:10:03 +0600

    v0.9.0

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,6 +1,12 @@
+'use client'
 import { VariantProps, cva } from 'class-variance-authority';
 import React, { HTMLProps } from 'react';
-import { cn } from '../../lib/utils';
+import { ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 const tableVariants = cva(
   'leading-10 bg-transparent',
@@ -11,9 +17,21 @@ const tableVariants = cva(
         bg: 'text-md bg-bg/20 tracking-tight p-5',
         border: 'text-md',
       },
+      padding: {
+        left: 'pl-5',
+        all: 'p-5',
+        right: 'pr-5',
+      },
+      alignmentment: {
+        left: 'text-left',
+        center: 'text-center',
+        right: 'text-right',
+      },
     },
     defaultVariants: {
       variant: 'default',
+      padding: null,
+      alignmentment: null
     },
   }
 );
@@ -22,36 +40,42 @@ const tableHeadVariants = cva(
   'm-0 bg-transparent text-tertiary p-0 even:bg-muted',
   {
     variants: {
-      align: {
+      variant:{
+        default: 'text-md bg-transparent border border-primary',
+        bg: 'text-md bg-bg/20 border-none',
+        border: 'text-md border border-primary',
+      },
+      alignment: {
         left: 'text-left',
         center: 'text-center',
         right: 'text-right',
       },
     },
     defaultVariants: {
-      align: 'left',
+      alignment: 'left',
+      variant: null
     },
   }
 );
 
-const tableRowVariants = cva(
-  'text-left bg-transparent text-tertiary even:bg-muted',
+const tableBodyVariants = cva(
+  'm-0 bg-transparent text-tertiary p-0 even:bg-muted',
   {
     variants: {
-      variant: {
-        default: 'text-md bg-transparent',
-        bg: 'text-md bg-bg/20 border-none even:bg-bg',
-        border: 'text-md border border-primary odd:bg-primary/10',
+      variant:{
+        default: 'text-md bg-transparent border border-primary',
+        bg: 'text-md bg-bg/20 border-none',
+        border: 'text-md border border-primary',
       },
-      align: {
+      alignment: {
         left: 'text-left',
         center: 'text-center',
         right: 'text-right',
       },
     },
     defaultVariants: {
-      align: 'left',
-      variant: 'default'
+      alignment: 'left',
+      variant: null
     },
   }
 );
@@ -99,53 +123,69 @@ const thVariants = cva(
   }
 );
 
+const trVariants = cva(
+  'text-bold text-primary p-0',
+  {
+    variants: {
+      variant:{
+        default: 'bg-primary/10 border-2 border-primary',
+        bg: 'text-md bg-bg/20 border-none',
+        border: 'text-md text-white bg-primary/50 border border-white',
+      },
+      padding: {
+        left: 'pl-5',
+        all: 'p-5',
+        right: 'pr-5',
+      },
+    },
+    defaultVariants: {
+      padding: 'all',
+    },
+  }
+);
+
 export interface TableProps extends HTMLProps<HTMLTableElement>, VariantProps<typeof tableVariants> {
   children: React.ReactNode;
   className?: string;
 }
 
-export const Table: React.FC<TableProps> = ({ children, variant, className, ...rest }) => {
+export const Table: React.FC<TableProps> = ({ children, padding, alignmentment, variant, className, ...rest }) => {
   return (
-    <table {...rest}>
-      <tbody>
-        <tr className={cn(tableVariants({ variant, className }))}>
-          {children}
-        </tr>
-      </tbody>
+    <table className={cn(tableVariants({ padding, alignmentment, variant, className }))} {...rest}>
+        {children}
     </table>
   );
 };
 
 export interface TableHeadProps extends HTMLProps<HTMLTableSectionElement>, VariantProps<typeof tableHeadVariants> {
-  children: React.ReactNode;
+  children: React.ReactNode | null;
   className?: string;
 }
 
-export const TableHead: React.FC<TableHeadProps> = ({ children, className, align, ...rest }) => {
+export const TableHead: React.FC<TableHeadProps> = ({ children, variant, className, alignment, ...rest }) => {
   return (
-    <thead {...rest}>
-      <tr className={cn(tableHeadVariants({ className, align }))}>
+    <thead className={cn(tableHeadVariants({ variant, className, alignment }))} {...rest}>
+
         {children}
-      </tr>
+
     </thead>
   );
 };
-
-export interface TableRowProps extends HTMLProps<HTMLTableRowElement>, VariantProps<typeof tableRowVariants> {
-  children: React.ReactNode;
+export interface TableBodyProps extends HTMLProps<HTMLTableSectionElement>, VariantProps<typeof tableBodyVariants> {
+  children: React.ReactNode | null;
   className?: string;
 }
 
-export const TableRow: React.FC<TableRowProps> = ({ children, variant, className, align, ...rest }) => {
+export const TableBody: React.FC<TableBodyProps> = ({ children, variant, className, alignment, ...rest }) => {
   return (
-    <tr className={cn(tableRowVariants({ className, variant, align }))} {...rest}>
+    <tbody className={cn(tableBodyVariants({ variant, className, alignment }))} {...rest}>
         {children}
-    </tr>
+    </tbody>
   );
 };
 /* TD */
 export interface tdProps extends HTMLProps<HTMLTableCellElement>, VariantProps<typeof tdVariants> {
-  children: React.ReactNode;
+  children: React.ReactNode | null;
   className?: string;
 }
 
@@ -158,7 +198,7 @@ export const Td: React.FC<tdProps> = ({ children, variant, className, padding, .
 };
 
 export interface thProps extends HTMLProps<HTMLTableCellElement>, VariantProps<typeof thVariants> {
-  children: React.ReactNode;
+  children: React.ReactNode | null;
   className?: string;
 }
 
@@ -168,4 +208,25 @@ export const Th: React.FC<thProps> = ({ children, variant, className, padding, .
         {children}
     </th>
   );
+};
+export interface trProps extends HTMLProps<HTMLTableRowElement>, VariantProps<typeof trVariants> {
+  children: React.ReactNode | null;
+  className?: string;
+}
+
+export const Tr: React.FC<trProps> = ({ children, variant, className, padding, ...rest }) => {
+  return (
+    <tr className={cn(trVariants({ className, variant, padding }))} {...rest}>
+        {children}
+    </tr>
+  );
+};
+
+export default {
+    Table,
+    TableHead,
+    TableBody,
+    Td,
+    Th,
+    Tr
 };
